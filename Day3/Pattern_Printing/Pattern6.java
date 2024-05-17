bebbae18f2a5e4099bba230100a14c1e123c2f25
dkartik2003 <dkartikachary@gmail.com>
2024-05-17 17:28:13 +0530

    Basics

--- Day3/Pattern_Printing/Pattern6.java
+++ Day3/Pattern_Printing/Pattern6.java
@@ -5,10 +5,20 @@ public class Pattern6 {
     pattern(5);
   }
   static void pattern(int n){
-    for(){
-      for(){
-        
+    int count=0;
+    for(int rows=0;rows<2*n-1;rows++){
+      for(int spaces=0;spaces<(2*rows)-(4*count);spaces++){
+System.out.print("  ");
       }
+    
+      
+      for(int stars=0;stars<(n-rows)+(2*count);stars++){
+System.out.print(" *");
+      }
+      if(rows>=n-1){
+        count+=1;
+      }
+      System.out.println();
     }
 
   }

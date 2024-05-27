beaaa5ef8d6e72f4e196b462e173c770cd594b24
dkartik2003 <dkartikachary@gmail.com>
2024-05-27 12:28:20 +0530

    OOPS

--- Day9/OOPS/Abstraction/Abstraction_Demo.java
+++ Day9/OOPS/Abstraction/Abstraction_Demo.java
@@ -12,5 +12,9 @@ public abstract class Abstraction_Demo {
 }
 //If a class is abstract then it may have abstract and non abstract method but an abstract method must be within abstract class.
 //An abstract class cannot have an object/child class.
+//Abstract class have constructor indirectly.
 //abstract method must be overriden in the child class.
 //Constructor chaining the process when constructor calls another constructor and so on this goes on then it is called so.
+
+
+//But in java 8 the functionality was changed and changed that we can create object of the abstract class and access the methods but this is not used widely.
--- Day9/OOPS/Abstraction/Abstraction_DemoClient.java
+++ Day9/OOPS/Abstraction/Abstraction_DemoClient.java
@@ -3,6 +3,7 @@ package Day9.OOPS.Abstraction;
 public class Abstraction_DemoClient {
   public static void main(String[] args) {
     Abstraction_DemoClient ss= new Abstraction_DemoClient();
+    ss.Payment();
     
   }
 }

fb79fb75ee7a3070df314b77301a2c396d50eda4
dkartik2003 <dkartikachary@gmail.com>
2024-05-28 17:30:13 +0530

    DS Afternoon

---
+++ Day10/DS/LinkedList/Leetcode876.java
@@ -0,0 +1,16 @@
+package Day10.DS.LinkedList;
+
+public class Leetcode876 {
+  LNode head;
+  LNode tail;
+
+  class LNode{
+    int val;
+    LNode next;
+    LNode(int val){
+      this.val=val;
+      
+    }
+  }
+  
+}

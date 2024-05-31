6ac91fbdf16fd3fa981eaa9c79b1512d5706cbb3
dkartik2003 <dkartikachary@gmail.com>
2024-05-31 17:42:25 +0530

    Queue

--- Day13/ArrayList/Arraylist1.java
+++ Day13/ArrayList/Arraylist1.java
@@ -2,6 +2,26 @@ package ArrayList;
 import java.util.*;
 public class Arraylist1 {
   public static void main(String[] args) {
-    ArrayList<Integer> list = new ArrayList<String>();
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(10);
+    list.add(10);
+
+    list.add(10);
+    list.add(10);
+
+    list.add(10);
+    list.add(10);
+    list.add(10);
+    list.add(10);
+    list.add(10);
+System.out.println(list.get(10));
+System.out.println(list);
+list.clear();
+list.add(10);
+list.add(10);
+list.add(10);
+list.addAll();
+list.clone();
+System.out.println(list.size());
   }
 }
